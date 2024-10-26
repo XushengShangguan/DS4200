1f54ecede8e8931aaff6276b11982d4b4e0731e9
Xusheng <shangguan.xu@northeastern.edu>
2024-10-25 21:12:00 -0400

    change names of files

--- Homework 3 template.js
+++ Homework 3 template.js
@@ -10,9 +10,17 @@ penguins.then(function(data) {
     });
 
     // Define the dimensions and margins for the SVG
+    const margin = { top: 20, right: 30, bottom: 50, left: 50 };
+    const width = 800 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
 
 
     // Create the SVG container
+    const svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
     
     
     // Set up scales for x and y axes
@@ -22,16 +30,50 @@ penguins.then(function(data) {
         .domain(data.map(d => d.Species))
         .range(d3.schemeCategory10);
 
-    // Add scales     
+    // Add scales
+    const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.PetalLength))
+    .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.PetalWidth))
+    .range([height, 0]);
+
     
 
     // Add circles for each data point
+    svg.selectAll("circle")
+    .data(data)
+    .enter().append("circle")
+    .attr("cx", d => xScale(d.PetalLength))
+    .attr("cy", d => yScale(d.PetalWidth))
+    .attr("r", 5)
+    .style("fill", d => colorScale(d.Species));
     
 
     // Add x-axis label
+    svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(xScale))
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", 40)
+    .style("fill", "black")
+    .style("text-anchor", "middle")
+    .text("Petal Length");
+
     
 
     // Add y-axis label
+    svg.append("g")
+    .call(d3.axisLeft(yScale))
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -40)
+    .attr("x", -height / 2)
+    .style("fill", "black")
+    .style("text-anchor", "middle")
+    .text("Petal Width");
     
 
     // Add legend
