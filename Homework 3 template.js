18d27dbb1fcb45db04721a90394c77b537404c09
Xusheng <shangguan.xu@northeastern.edu>
2024-10-25 22:28:14 -0400

    change js

--- Homework 3 template.js
+++ Homework 3 template.js
@@ -96,15 +96,13 @@ iris.then(function(data) {
 
 });
 
-penguins.then(function(data) {
+iris.then(function(data) {
     // Convert string values to numbers
     data.forEach(function(d) {
         d.PetalLength = +d.PetalLength;
         d.PetalWidth = +d.PetalWidth;
     });
-
     
-
     // Define the dimensions and margins for the SVG
     const margin = { top: 20, right: 30, bottom: 50, left: 50 };
     const width = 800 - margin.left - margin.right;
@@ -166,7 +164,7 @@ penguins.then(function(data) {
         const x = xScale(Species);
         const boxWidth = xScale.bandwidth();
         const IQR = quartiles.q3 - quartiles.q1;
-        
+
         // Draw vertical lines
         svg.append("line")
             .attr("x1", x + boxWidth / 2)
